d59acaf15ac8fa6576c52720dd16a3dbe45e3d9b
rikydago <rikydago03@gmail.com>
2025-01-29 19:19:56 +0100

    OrdineController.cs, non ho avuto molto tempo dopo le 18, domani mattina facciamo gli altri

---
+++ GestionaleNegozio/Controllers/OrdineController.cs
@@ -0,0 +1,73 @@
+﻿using GestionaleNegozio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class OrdineController : BaseController
+{
+    private readonly OrdineDao _ordineDao;
+
+    public OrdineController()
+    {
+        _ordineDao = new OrdineDao(_connectionString);
+    }
+
+    public ActionResult Index()
+    {
+        var negozi = _ordineDao.GetAll();
+        return View(negozi);
+    }
+
+    public ActionResult Details(int id)
+    {
+        var ordine = _ordineDao.GetById(id);
+        if (ordine == null) return NotFound();
+        return View(ordine);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Ordine ordine)
+    {
+        if (ModelState.IsValid)
+        {
+            _ordineDao.Insert(ordine);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(ordine);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var ordine = _ordineDao.GetById(id);
+        if (ordine == null) return NotFound();
+        return View(ordine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Ordine ordine)
+    {
+        if (ModelState.IsValid)
+        {
+            _ordineDao.Update(ordine);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(ordine);
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var ordine = _ordineDao.GetById(id);
+        if (ordine == null) return NotFound();
+        return View(ordine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        _ordineDao.Delete(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
