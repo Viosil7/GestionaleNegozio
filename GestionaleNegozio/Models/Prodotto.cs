b4bd0528a8b9cc0abbde3317f43be29e059d8ca6
calon96 <noivoi.tu5@gmail.com>
2025-01-31 17:59:22 +0100

    fix prezzo

--- GestionaleNegozio/Models/Prodotto.cs
+++ GestionaleNegozio/Models/Prodotto.cs
@@ -1,4 +1,6 @@
-﻿namespace GestionaleNegozio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionaleNegozio.Models
 {
     public class Prodotto
     {
@@ -6,6 +8,7 @@
         public string Nome { get; set; }
         public string Categoria { get; set; }
         public string Descrizione { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal Prezzo { get; set; }
     }
 }
