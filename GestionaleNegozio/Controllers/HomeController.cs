b91fc2b1ed7690a067e6a887a4dc263e918cae40
calon96 <noivoi.tu5@gmail.com>
2025-02-01 18:47:36 +0100

    Resa come pagina iniziale quella di LogIn se l'utente non è loggato.

--- GestionaleNegozio/Controllers/HomeController.cs
+++ GestionaleNegozio/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestionaleNegozio.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [AllowAnonymous]
         public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
         {
             return View();
         }
