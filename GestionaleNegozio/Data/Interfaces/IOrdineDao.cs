b5a95dd9d6158fd183714be9dfe7cddcbfdc0828
ludozatt <ludozatt@yahoo.it>
2025-02-03 12:03:18 +0100

    aggiunto namespace in IOrdineDao

--- GestionaleNegozio/Data/Interfaces/IOrdineDao.cs
+++ GestionaleNegozio/Data/Interfaces/IOrdineDao.cs
@@ -1,16 +1,19 @@
 ﻿using GestionaleNegozio.Models;
 
-public interface IOrdineDao
+namespace GestionaleNegozio.Data.Interfaces
 {
-    List<OrderViewModel> GetAll();
-    OrderViewModel GetById(int id);
-    int Insert(OrderViewModel order);
-    void Update(OrderViewModel order);
-    void Delete(int id);
-    void DeleteOrderItems(int orderId);
-    List<OrderViewModel> GetByNegozio(int idNegozio);
-    List<OrderViewModel> GetByDateRange(DateTime startDate, DateTime endDate);
-    List<OrderViewModel> GetByProdotto(int idProdotto);
-    List<OrderViewModel> GetRecentOrders(int limit = 10);
-    bool Exists(int id);
+    public interface IOrdineDao
+    {
+        List<OrderViewModel> GetAll();
+        OrderViewModel GetById(int id);
+        int Insert(OrderViewModel order);
+        void Update(OrderViewModel order);
+        void Delete(int id);
+        void DeleteOrderItems(int orderId);
+        List<OrderViewModel> GetByNegozio(int idNegozio);
+        List<OrderViewModel> GetByDateRange(DateTime startDate, DateTime endDate);
+        List<OrderViewModel> GetByProdotto(int idProdotto);
+        List<OrderViewModel> GetRecentOrders(int limit = 10);
+        bool Exists(int id);
+    }
 }
